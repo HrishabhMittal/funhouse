bf17b7c9092cf4e74dfdc32b751edc2ef3977d35
HrishabhMittal <hrishabhmittal.hm@gmail.com>
2026-03-19 04:30:02 +0530

    remove unnecessary stuff, fix rain

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Background from './components/Background';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-base text-textMain font-mono leading-relaxed pb-12">
-      <Header />
-      <Hero />
-      <main className="max-w-[800px] mx-auto px-5 flex flex-col gap-10">
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-      </main>
+    <div className="min-h-screen text-textMain font-mono leading-relaxed pb-12">
+      <Background />
+      <div className="relative z-10 flex flex-col gap-10">
+        <Header />
+        <Hero />
+        <main className="max-w-[800px] w-full mx-auto px-5 flex flex-col gap-10">
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+        </main>
+      </div>
     </div>
   );
 }
